637f4bf04b4cb2a52001fd603a363a612dd2732b
Sean Sundberg <seansund@us.ibm.com>
2020-11-12 16:23:27 -0600

    Adds ibmcloud login to verify scripts

--- .github/scripts/validate-deploy.sh
+++ .github/scripts/validate-deploy.sh
@@ -7,6 +7,8 @@ PUBLIC_GATEWAY="$2"
 
 VPC_NAME="${PREFIX_NAME}-vpc"
 
+ibmcloud login -r "${TF_VAR_region}" -g "${TF_VAR_resource_group_name}" --apikey "${TF_VAR_ibmcloud_api_key}"
+
 set -e
 
 VPC_ID=$(ibmcloud is vpcs | grep "${VPC_NAME}" | sed -E "s/^([A-Za-z0-9-]+).*/\1/g")
