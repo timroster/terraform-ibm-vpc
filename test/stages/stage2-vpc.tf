1bda04f6c8db47f0350a551c9e7a080bc03c41e9
Sean Sundberg <seansund@us.ibm.com>
2021-04-02 08:44:33 -0500

    Adds resource group dependency (#5)
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- test/stages/stage2-vpc.tf
+++ test/stages/stage2-vpc.tf
@@ -1,7 +1,7 @@
 module "dev_vpc" {
   source = "./module"
 
-  resource_group_name = var.resource_group_name
+  resource_group_name = module.resource_group.name
   region              = var.region
   name_prefix         = var.name_prefix
   ibmcloud_api_key    = var.ibmcloud_api_key
