9c6a6fa64281d33e8fd9a20f6a8d93ee43c0b969
Sean Sundberg <seansund@us.ibm.com>
2021-04-08 16:18:19 -0500

    Restores default_security_group_name and sets min version to 1.22.0 (#13)
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- version.tf
+++ version.tf
@@ -1,10 +1,10 @@
 terraform {
-  required_version = ">= 0.12.0"
+  required_version = ">= 0.13.0"
 
   required_providers {
     ibm = {
       source = "ibm-cloud/ibm"
-      version = ">= 1.17"
+      version = ">= 1.22.0"
     }
   }
 }
