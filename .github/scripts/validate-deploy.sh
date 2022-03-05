8da60d7190b7c295979e62b97c2496bf7dd7e862
Sean Sundberg <seansund@us.ibm.com>
2022-03-05 13:56:21 -0600

    Updates test to bring in clis using cli submodule (#56)
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- .github/scripts/validate-deploy.sh
+++ .github/scripts/validate-deploy.sh
@@ -2,6 +2,8 @@
 
 SCRIPT_DIR=$(cd $(dirname "$0"); pwd -P)
 
+BIN_DIR=$(cat .bin_dir)
+
 echo "terraform.tfvars"
 cat terraform.tfvars
 
@@ -16,10 +18,10 @@ echo "IBMCLOUD_API_KEY: ${IBMCLOUD_API_KEY}"
 
 VPC_NAME="${PREFIX_NAME}-vpc"
 
-ibmcloud login -r "${REGION}" -g "${RESOURCE_GROUP_NAME}" --apikey "${IBMCLOUD_API_KEY}"
+${BIN_DIR}/ibmcloud login -r "${REGION}" -g "${RESOURCE_GROUP_NAME}" --apikey "${IBMCLOUD_API_KEY}"
 
 echo "Retrieving VPC_ID for name: ${VPC_NAME}"
-VPC_ID=$(ibmcloud is vpcs | grep "${VPC_NAME}" | sed -E "s/^([A-Za-z0-9-]+).*/\1/g")
+VPC_ID=$(${BIN_DIR}/ibmcloud is vpcs | grep "${VPC_NAME}" | sed -E "s/^([A-Za-z0-9-]+).*/\1/g")
 
 if [[ -z "${VPC_ID}" ]]; then
   echo "VPC id not found: ${VPC_NAME}"
@@ -27,8 +29,8 @@ if [[ -z "${VPC_ID}" ]]; then
 fi
 
 echo "Retrieving VPC info for id: ${VPC_ID}"
-ibmcloud is vpc "${VPC_ID}"
-if ! ibmcloud is vpc "${VPC_ID}"; then
+${BIN_DIR}/ibmcloud is vpc "${VPC_ID}"
+if ! ${BIN_DIR}/ibmcloud is vpc "${VPC_ID}"; then
   echo "Unable to find vpc for id: ${VPC_ID}"
   exit 1
 fi
@@ -36,8 +38,8 @@ fi
 SG_NAME="${VPC_NAME}-base"
 
 echo "Testing security group rules"
-ibmcloud is security-groups --output JSON | jq '.[]'
-OPEN_RULES=$(ibmcloud is security-groups --output JSON | jq -c --arg SG_NAME "${SG_NAME}" '.[] | select(.name == $SG_NAME) | .rules[]')
+${BIN_DIR}/ibmcloud is security-groups --output JSON | ${BIN_DIR}/jq '.[]'
+OPEN_RULES=$(${BIN_DIR}/ibmcloud is security-groups --output JSON | ${BIN_DIR}/jq -c --arg SG_NAME "${SG_NAME}" '.[] | select(.name == $SG_NAME) | .rules[]')
 if [[ -z "${OPEN_RULES}" ]]; then
   echo "No rules found for '${SG_NAME}'"
   exit 1
